5a0ea6543042fedcfb04bdb522825fe0c962adab
acopalypse <acopalypse@yandex.ru>
2021-04-26 16:02:47 +0300

    edit if floor/ceil block

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ function binarySearch(number, index, step, side) {
   if (step > 6 || number < 2) {
     document.getElementById("error").innerHTML = `
       <p>
-        Что то пошло не так...<br> Кол-во шагов не может превышать 7.
-        И число не должно быть меньше 1.
+        Что то пошло не так...
+        <br>Кол-во шагов не может превышать 7.
+        <br>И число не должно быть меньше 1.
       </p>
     `;
     document.getElementById("inner").innerHTML = `
@@ -12,22 +13,21 @@ function binarySearch(number, index, step, side) {
       </div>
     `;
   } else {
-    if (side) {
-      index = Math.floor(index / 2);
-    } else {
-      index = Math.ceil(index / 2);
-    }
-    number -= index;
+    side == 1
+      ? (index = Math.floor(index / 2))
+      : (index = Math.ceil(index / 2));
     step += 1;
     document.getElementById("inner").innerHTML = `
     <p>Шаг ${step}</p>
-    <p>Число ${number}</p>
+    <p>Число ${number - index}</p>
     <div class="buttons">
-      <button onclick="binarySearch(${number}, ${index}, ${step}, 0)"> < Меньше</button>
-      <button onclick="binarySearchEnd(${number}, ${step})">Угадал!</button>
       <button onclick="binarySearch(${
-        number + index
-      }, ${index}, ${step}, 1)">Больше > </button>
+        number - index
+      }, ${index}, ${step}, 0)"> < Меньше</button>
+      <button onclick="binarySearchEnd(${
+        number - index
+      }, ${step})">Угадал!</button>
+      <button onclick="binarySearch(${number}, ${index}, ${step}, 1)">Больше > </button>
     </div>`;
     document.getElementById("error").innerHTML = "";
   }
